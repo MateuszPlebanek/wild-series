c2eadd82b99cf67be2b70f6073b123d472875a36
MateuszPlebanek <matprocom@gmail.com>
2025-02-06 23:07:03 +0100

    Ajout de la fonctionnalité d'affichage des séries

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,13 +8,16 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
-import sayActions from "./modules/say/sayActions";
-import programActions from "./modules/programs/programActions";
-
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+import programActions from "./modules/programs/programActions";
 router.get("/api/programs", programActions.browse);
+router.get("/api/programs/:id", programActions.read);
+
+import sayActions from "./modules/say/sayActions";
+
 /* ************************************************************************* */
 // Declaration of a "Welcome" route
 
@@ -23,7 +26,7 @@ router.get("/", sayActions.sayWelcome);
 import type { RequestHandler } from "express";
 
 const sayWelcome: RequestHandler = (req, res) => {
-    res.send("Welcome to Wild Series !");
+  res.send("Welcome to Wild Series !");
 };
 
 router.get("/", sayWelcome);
